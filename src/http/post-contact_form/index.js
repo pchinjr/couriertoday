1ba85f3cb65d6ed59a430d6106ff56967afa6336
Paul Chin Jr <pchinjr@gmail.com>
2020-09-16 15:47:40 -0400

    time for a commit

--- src/http/post-contact_form/index.js
+++ src/http/post-contact_form/index.js
@@ -1,9 +1,44 @@
+let tiny = require('tiny-json-http')
 let arc = require('@architect/functions')
 
-exports.handler = async function http(req) {
-  let body = arc.http.helpers.bodyParser(req)
-  console.table(body)
+
+let sendCourier = async function (req) {
+  let body = req.body
+
+  let url = 'https://api.trycourier.app/send'
+
+  let headers = {
+    "authorization": `Bearer ${process.env.COURIER_AUTH_TOKEN}`,
+    "content-type": "application/json",
+  }
+
+  let data = {
+    event: `praisecage-contact`,
+    recipient: 'pchinjr',
+    profile: {
+      slack: {
+        access_token: `${process.env.SLACK_TOKEN}`,
+        email: 'pchinjr@gmail.com'
+      }
+    },
+    data: {
+      name: `${body.name}`,
+      email: `${body.email}`,
+      subject: `${body.subject}`,
+      message: `${body.message}`,
+      urgent: `${body.urgent}`
+    }
+  }
+
+  let response = await tiny.post({ url, data, headers })
+
+  console.log(response)
+}
+
+let route = async function (req) {
   return {
-    body: `praise cage`
+    location: '/'
   }
 }
+
+exports.handler = arc.http.async(sendCourier, route)
