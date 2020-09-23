a5fc5efd91637b6fff20088ba8ecd29f4aec3b77
Paul Chin Jr <pchinjr@gmail.com>
2020-09-23 09:25:55 -0400

    api url update

--- src/http/post-contact_form/index.js
+++ src/http/post-contact_form/index.js
@@ -5,7 +5,7 @@ let arc = require('@architect/functions')
 let sendCourier = async function (req) {
   let body = req.body
 
-  let url = 'https://api.trycourier.app/send'
+  let url = 'https://api.courier.com/send'
 
   let headers = {
     "authorization": `Bearer ${process.env.COURIER_AUTH_TOKEN}`,
